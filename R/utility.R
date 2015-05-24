1d122b432c667f5aafbb4779bd5ffa4ea6419e94
WilliamKinaan <w.kinaan@gmail.com>
2015-05-24 15:35:59 +0100

    Until now

--- R/utility.R
+++ R/utility.R
@@ -38,6 +38,13 @@ isCorrectDateTime <- function(dateTime){
   }
 }
 
+#' Is Correct Time
+#'
+#' Input is a vector of integer/string values to be validated
+#' @param time: a vector of integer/string values
+#' @description Check if all the values of the input vectors have this format %H%M
+#' @return TRUE if all the values of the vector have the correct format, and otherwise it returns FALSE
+#' @export 
 isCorrectTime <- function(time){
   dt <- strptime(time, "%H%M")
   bad <- which(is.na(dt))
@@ -49,7 +56,13 @@ isCorrectTime <- function(time){
 }
 
 
-
+#' Wrong Time Values
+#'
+#' Input is a vector of integer/string values
+#' @param time: a vector of integer/string values
+#' @description Returns the wrong time values and their indexes.
+#' @return a data frame contains two columns; WrongValues is a vector contains the wrong values, and WrongIndexes contains the indexes of the wrong values
+#' @export
 wrongTimeValues <- function(time){
   dt<-strptime(time, " %H%M")
   bad<-which(is.na(dt))
@@ -60,7 +73,13 @@ lengthOfIntegerValue <- function(oneValue){
   return (nchar(as.character(oneValue)))
 }
 
-
+#' Is the length of all time values equal to three
+#'
+#' Input is a vector of integer/string values
+#' @param time: a vector of integer/string values
+#' @description Check if the length of all the values equal to three, we need that in order to know situations where the the leff zero is missing in the time value
+#' @return TRUE when the lenght of all the values equal to three, and FALSE in the otherwise
+#' @export
 isTheLengthOfAllTimeValuesThree <-function(time){
   lengthVector = lapply(X = time, FUN = lengthOfIntegerValue)
   return (all(lengthVector == 3))
@@ -80,19 +99,37 @@ correctMissingLeftZeroForOneValue <- function (oneValue){
   return (oneValue)
 }
 
-
+#' Correct the missing zero at the left of a time value
+#'
+#' Input is a vector of integer/string values
+#' @param time: a vector of integer/string values
+#' @description There are some time values have this format 921 which should be 0921 but because it is integer values, the zero on the left will not appear. This function is to handle these cases
+#' @return a vector of CHARACTORS contain the values with the correct format.
+#' @export
 correctMissingLeftZeroForTime <- function (time){
   return (lapply(X = time, FUN = correctMissingLeftZeroForOneValue))
 }
 
-
+#' Are the values of one vector contained in another vector
+#'
+#' It is like foreign key/primary key validation in relational database.
+#' @param foreignKeys: vector contains the values that we need to validation
+#' @param primaryKeys: vector contains the values that we need to validation throw out
+#' @description Use this function to validation that the values of one vector are already existed in another vector. If this function returns FALSE, you can use the function notExistedValues to see which values are existed in the first vector but not in the second vector
+#' @return  TRUE if all the values of the first vector are included in the second vector, and FALSE in the otherwise case.
+#' @export
 areValuesExisted <- function (foriegnKeys, primaryKeys){
   return (all(foriegnKeys %in% primaryKeys))
 }
 
-
+#' Not existed values
+#'
+#' @param foreignKeys: vector contains the values that we need to validation
+#' @param primaryKeys: vector contains the values that we need to validation throw out
+#' @description This function returns the values that exist in the first vector but don't exist in the second one
+#' @export
 notExistedValues <- function(foreignKeys, primaryKeys){
-  differencesBooleanVector1 <- foreignKeys %in% primaryKeys
-  return (foreignKeys[!differencesBooleanVector1])
+  differencesBooleanVector <- foreignKeys %in% primaryKeys
+  return (foreignKeys[!differencesBooleanVector])
 }
 
