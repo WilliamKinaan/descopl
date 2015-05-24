6c6ead3d3c8cc095de053f0ea11794011a395807
WilliamKinaan <w.kinaan@gmail.com>
2015-05-24 14:28:22 +0100

    jkjllkjlkjlk

--- R/utility.R
+++ R/utility.R
@@ -2,7 +2,7 @@
 #'
 #' Input is a vector of integer/string values to be validated
 #' @param date: a vector of integer/string values
-#' @description Check if all the values of the input vectors have this format %Y%m%d
+#' @description Check if all the values of the input vectors have this format
 #' @return TRUE if all the values of the vector have the correct format, and otherwise it returns FALSE
 #' @export 
 isCorrectedDate <- function(date){
@@ -110,7 +110,14 @@ correctMissingLeftZeroForTime <- function (time){
   return (lapply(X = time, FUN = correctMissingLeftZeroForOneValue))
 }
 
-
+#' Are the values of one vector contained in another vector
+#'
+#' It is like foreign key/primary key validation in relational database.
+#' @param foreignKeys: vector contains the values that we need to validation
+#' @param primaryKeys: vector contains the values that we need to validation throw out
+#' @description Use this function to validation that the values of one vector are already existed in another vector. If this function returns FALSE, you can use the function notExistedValues to see which values are existed in the first vector but not in the second vector
+#' @return  TRUE if all the values of the first vector are included in the second vector, and FALSE in the otherwise case.
+#' @export
 areValuesExisted <- function (foriegnKeys, primaryKeys){
   return (all(foriegnKeys %in% primaryKeys))
 }
